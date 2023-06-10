8e6ed645712c7f49d878639fb3a2b640ff8b02b8
rvhb-killer <114177579+rvhb-killer@users.noreply.github.com>
2023-06-10 20:58:52 +0200

    Update script.js
    Signed-off-by: rvhb-killer <114177579+rvhb-killer@users.noreply.github.com>

--- script.js
+++ script.js
@@ -33,4 +33,43 @@ $(document).ready(function() {
             // Add your scroll-related code here
         });
     });
+
+
+  /** ---------------------------- //
+   *  @group viewport trigger script 
+   * for adding or removing classes from elements in view within viewport
+   */
+
+  var $animationElements = $('.animation-element');
+  var $window = $(window);
+
+  // ps: Let's FIRST disable triggering on small devices!
+  var isMobile = window.matchMedia("only screen and (max-width: 768px)");
+  if (isMobile.matches) {
+    $animationElements.removeClass('animation-element');
+  }
+
+  function checkIfInView() {
+    var windowHeight = $window.height();
+    var windowTopPosition = $window.scrollTop();
+    var windowBottomPosition = (windowTopPosition + windowHeight);
+
+    $.each($animationElements, function() {
+      var $element = $(this);
+      var elementHeight = $element.outerHeight();
+      var elementTopPosition = $element.offset().top;
+      var elementBottomPosition = (elementTopPosition + elementHeight);
+
+      // check to see if this current container is within viewport
+      if ((elementBottomPosition >= windowTopPosition) && (elementTopPosition <= windowBottomPosition)) {
+        $element.addClass('heading-largeoverlay');
+      } else {
+        $element.removeClass('heading-largeoverlay');
+      }
+    });
+  }
+
+  // Bind the scroll and resize events outside the $.each loop
+  $window.on('scroll resize', checkIfInView);
+  $window.trigger('scroll');
 });
